93dad3ddd3f3e0f38f223afb92e5dbf39665573f
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-05 13:49:04 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_StaticVariableMethod
+{
+    public class Employee
+    {
+
+        static int count;
+        private int empid;
+        private string name;
+        private string address;
+
+        public Employee(string name, string address)
+        {
+
+            count++;
+            empid = count;
+            this.name = name;
+            this.address = address;
+
+            this.address = address;
+
+
+
+        }
+        public static int GetCount()
+        {
+            return count;
+
+
+        }
+
+
+
+        public string GetData()
+        {
+            return "emp_id" + empid + "empname" + name + "empaddress" + address;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_StaticVariableMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee("Shubhankar", "Solapur");
+            Console.WriteLine((emp.GetData()));
+            Console.WriteLine(emp.Get);
+            Console.ReadLine();
+        }
+    }
+}
+
